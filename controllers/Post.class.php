6a544a82cda5f452637c539a7f4504a13b1088b4
Kruzei <naufal.michiels@gmail.com>
2024-11-03 21:39:33 +0700

    chore: remove comments

--- controllers/Post.class.php
+++ controllers/Post.class.php
@@ -3,13 +3,10 @@
 
     public function index()
     {
-        // Load model
         $postModel = $this->loadModel('PostModel');
 
-        // Get data from the model
         $posts = $postModel->getAll();
 
-        // Load the view
         $this->loadView('posts', ['posts' => $posts]);
     }
 
